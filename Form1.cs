680281f7566e8f4e937151fc967f1ca5470e2793
Jørgen <85877485+Kvikku@users.noreply.github.com>
2023-02-01 10:31:48 +0100

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Net;
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+//TO DO
+
+///// UI ///////////////////
+
+// Flat theme
+// 
+
+
+///// Functionality /////
+
+// List all assignments with intent for an application
+// Add one or more assignments with intent for an application
+// Delete one or more assignments for an application
+
+
+
+namespace IntuneAssignments
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
