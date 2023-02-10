37de84b7ec1defc233541184a140224cec62ff3d
Jørgen <85877485+Kvikku@users.noreply.github.com>
2023-02-10 13:51:30 +0100

    finished MSAL login window

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblTenantID = new System.Windows.Forms.Label();
+            this.lblTenantInfo = new System.Windows.Forms.Label();
+            this.lblSignedInUser = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblTenantID);
+            this.panel1.Controls.Add(this.lblTenantInfo);
+            this.panel1.Controls.Add(this.lblSignedInUser);
+            this.panel1.Location = new System.Drawing.Point(762, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(309, 183);
+            this.panel1.TabIndex = 0;
+            // 
+            // lblTenantID
+            // 
+            this.lblTenantID.AutoSize = true;
+            this.lblTenantID.ForeColor = System.Drawing.Color.Salmon;
+            this.lblTenantID.Location = new System.Drawing.Point(12, 74);
+            this.lblTenantID.Name = "lblTenantID";
+            this.lblTenantID.Size = new System.Drawing.Size(62, 15);
+            this.lblTenantID.TabIndex = 3;
+            this.lblTenantID.Text = "TENANTID";
+            // 
+            // lblTenantInfo
+            // 
+            this.lblTenantInfo.AutoSize = true;
+            this.lblTenantInfo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTenantInfo.ForeColor = System.Drawing.Color.Salmon;
+            this.lblTenantInfo.Location = new System.Drawing.Point(12, 13);
+            this.lblTenantInfo.Name = "lblTenantInfo";
+            this.lblTenantInfo.Size = new System.Drawing.Size(113, 25);
+            this.lblTenantInfo.TabIndex = 2;
+            this.lblTenantInfo.Text = "Tenant info";
+            // 
+            // lblSignedInUser
+            // 
+            this.lblSignedInUser.AutoSize = true;
+            this.lblSignedInUser.ForeColor = System.Drawing.Color.Salmon;
+            this.lblSignedInUser.Location = new System.Drawing.Point(12, 48);
+            this.lblSignedInUser.Name = "lblSignedInUser";
+            this.lblSignedInUser.Size = new System.Drawing.Size(92, 15);
+            this.lblSignedInUser.TabIndex = 1;
+            this.lblSignedInUser.Text = "SIGNED IN USER";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(387, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(951, 741);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "WIP";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(966, 586);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(1083, 794);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Button button2;
+        private Label lblSignedInUser;
+        private Label lblTenantInfo;
+        private Label lblTenantID;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,14 +5,22 @@ using Microsoft.Identity.Client;
 using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Azure.Identity;
+using static System.Formats.Asn1.AsnWriter;
+using System.Security.Principal;
+using System.Threading.Tasks;
 
 //TO DO
 
 ///// UI ///////////////////
 
 // Flat theme
-// 
+// Animations
 
+
+///// Framework ////////////
+
+// Everything as simple methods
 
 ///// Functionality /////
 
@@ -31,9 +39,133 @@ namespace IntuneAssignments
             InitializeComponent();
         }
 
+        // Global variables //
+
+        string clientID = "f67679c6-4a23-42d8-84c6-bb3f9cf1f1c0";
+        string tenantID = "18456af8-4036-4e1c-b888-43e04c49046a";
+        string clientSecret = "";
+        string[] scopes = new string[] { "Calendars.Read", "Calendars.ReadWrite" };
+        string GraphEndpoint = "https://graph.microsoft.com/v1.0";
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            //AuthenticateToGraph();
+            
+
         }
+
+        // Methods //
+
+        // Authentication
+
+
+        // With client secret //////////////////////////////////////////////////////////////////////////////////////
+        private async Task AuthenticateToGraph()
+        {
+            InteractiveBrowserCredential interactiveBrowserCredential = new InteractiveBrowserCredential();
+            var graphClient = new GraphServiceClient(interactiveBrowserCredential);
+
+            InteractiveBrowserCredentialOptions options = new InteractiveBrowserCredentialOptions();
+
+            try
+            {
+                // Make a call to Microsoft Graph to confirm that authentication was successful
+                var me = await graphClient.Me
+                    .Request()
+                    .GetAsync();
+
+                var tenantInfo = await graphClient.Organization
+                    .Request()
+                    .Select("ID")
+                    .GetAsync();
+                
+                List<Organization> organizations = new List<Organization>();
+                organizations.AddRange(tenantInfo);
+
+                foreach (var org in organizations)
+                {
+                    
+                    tenantID= org.Id;
+                    lblTenantID.Text = "Tenant ID: " + tenantID;
+                }
+
+                lblSignedInUser.Text = "Signed in as " + me.DisplayName;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error authenticating: " + ex.Message);
+            }
+        }
+
+        // With username and password ///////////////////////////////////////////////////////////////////////////////
+        private async Task authMSAL()
+        {
+            var app = PublicClientApplicationBuilder.Create(clientID)
+                           .WithAuthority(AzureCloudInstance.AzurePublic, tenantID)
+                           .Build();
+
+            var result = await app.AcquireTokenInteractive(scopes)
+                                   .WithUseEmbeddedWebView(true)
+                                   .WithPrompt(Microsoft.Identity.Client.Prompt.SelectAccount)
+                                   .ExecuteAsync();
+
+            // Use the result to create a GraphServiceClient
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                        return Task.FromResult(0);
+                    }));
+
+            // Make a call to Microsoft Graph
+            var me = await graphClient.Me.Request().GetAsync();
+
+            lblSignedInUser.Text = me.DisplayName;
+
+            var tenantInfo = await graphClient.Organization
+                    .Request()
+                    .Select("ID")
+                    .GetAsync();
+
+            List<Organization> organizations = new List<Organization>();
+            organizations.AddRange(tenantInfo);
+
+            foreach (var org in organizations)
+            {
+
+                tenantID = org.Id;
+                lblTenantID.Text = "Tenant ID: " + tenantID;
+            }
+
+        }
+
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await authMSAL();
+        }
+
+
+        
+
+
+
+
+        ///////////////////////////////////////////////////////////////////////
+        /// Work in progress ///
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+
     }
-}
+
+        ///////////////////////////////////////////////////////////////////////
+    }
