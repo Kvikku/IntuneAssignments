64c9ecda216a19a3b61db563bc8107280db1e8bd
Jørgen <85877485+Kvikku@users.noreply.github.com>
2023-11-17 11:51:37 +0100

    Begun home page creation on seperate branch

--- HomePage.Designer.cs
+++ HomePage.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(93, 118);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 0;
+            label1.Text = "TEST";
             // 
             // HomePage
             // 
@@ -36,13 +46,17 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(46, 51, 73);
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
             Enabled = false;
             ForeColor = Color.Black;
             Name = "HomePage";
             Text = "HomePage";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
