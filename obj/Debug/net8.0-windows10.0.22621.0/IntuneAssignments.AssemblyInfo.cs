2aec2b4ea6a19698f28398a31c5aac4ae1ea6c32
Jørgen <85877485+Kvikku@users.noreply.github.com>
2024-05-06 12:53:45 +0200

    Begun cleaning up folder structure

--- obj/Debug/net8.0-windows10.0.22621.0/IntuneAssignments.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.22621.0/IntuneAssignments.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntuneAssignments")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+157c525bff43bb542bbe20d789d119a6db9fd844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5561586072137ab6c8242f9a903ba082054aa79")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntuneAssignments")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntuneAssignments")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
