83a84fdab8ae00650d3053dfd5ebc82a83b514a7
Jørgen <85877485+Kvikku@users.noreply.github.com>
2024-06-07 13:18:57 +0200

    Update to .gitignore file

--- obj/Debug/net8.0-windows10.0.22621.0/IntuneAssignments.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.22621.0/IntuneAssignments.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntuneAssignments")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d97367c2e5fac09326bdc156f191257b222ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f071f84c4cf5343adbe89ed98682000801225f92")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntuneAssignments")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntuneAssignments")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
