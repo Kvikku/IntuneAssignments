5b460a46ce492f7b8bac473f9b27f34c5f6e5b2b
Jørgen Sundet <85877485+Kvikku@users.noreply.github.com>
2025-04-08 13:42:34 +0200

    Add ADMXtemplates class with necessary using directives
    Introduces a new `ADMXtemplates` class in the
    `IntuneAssignments.Backend.IntuneContentClasses` namespace.
    Includes several using directives for required namespaces
    like `System` and `Microsoft.Graph`. The class is currently
    empty and does not contain any members or methods.

---
+++ Backend/IntuneContentClasses/ADMXtemplates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.Graph.Beta;
+using Microsoft.Graph.Beta.Models;
+using static IntuneAssignments.Backend.Utilities.FormUtilities;
+using static IntuneAssignments.Backend.Utilities.GlobalVariables;
+using static IntuneAssignments.Backend.IntuneContentClasses.Filters;
+
+namespace IntuneAssignments.Backend.IntuneContentClasses
+{
+    public class ADMXtemplates
+    {
+
+    }
+}
